f62ebee30b7f21bc0ef09e73c117f9fc96082aa9
rustam <darvishi123@yandex.ru>
2023-02-10 21:37:47 +0300

    add currency keyboard

--- keyboards/default_keyboards.py
+++ keyboards/default_keyboards.py
@@ -6,6 +6,18 @@ button2 = KeyboardButton('/Добавить_расход')
 button3 = KeyboardButton('/Отправить_статистику')
 button4 = KeyboardButton('/Показать_категории_расходов')
 
-kb_client = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+kb_client = ReplyKeyboardMarkup(resize_keyboard=True)
 
 kb_client.add(button1).add(button2).insert(button3).add(button4)
+
+
+button1 = KeyboardButton('$')
+button2 = KeyboardButton('€')
+button3 = KeyboardButton('₽')
+button4 = KeyboardButton('бел. рубли')
+
+currency_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+
+currency_kb.add(button1).add(button2).add(button3).add(button4)
+
+
