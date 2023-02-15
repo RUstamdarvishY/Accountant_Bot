8e51b63697cbc8966232b942721174917a4d3956
rustam <darvishi123@yandex.ru>
2023-02-15 18:36:12 +0300

    fixing db models

--- utils/db_api/models.py
+++ utils/db_api/models.py
@@ -4,8 +4,8 @@ from sqlalchemy.orm import relationship
 from decouple import config
 
 
-DB_USER = config('DB_USER') 
-DB_PASS = config('DB_PASS') 
+DB_USER = config('DB_USER')
+DB_PASS = config('DB_PASS')
 connection_string = f'postgresql://{DB_USER}:{DB_PASS}@localhost/bot_base'
 
 
@@ -13,36 +13,35 @@ engine = create_engine(connection_string)
 
 Base = declarative_base()
 
-class User(Base):
-    __tablename__ = 'users'
 
-    user_id = Column(Integer, primary_key=True)
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
     username = Column(String(255), nullable=False)
     telegram_id = Column(Integer, nullable=False)
-    email = Column()
-    expenses = relationship("Expense", back_populates="users")
+    email = Column(String(255))
+    expenses = relationship('Expense', backref='user')
 
 
 class Expense(Base):
-    __tablename__ = 'expenses'
+    __tablename__ = 'expense'
 
-    expense_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     time = Column(DateTime)
     price = Column(Float, nullable=False)
     currency = Column(String(20), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
     category_id = Column(Integer, ForeignKey('category.id'))
-    categories = relationship("Category", back_populates="expenses")
-    user_id = Column(Integer, ForeignKey("user.id"))
-    users = relationship("User", back_populates="expenses")
 
 
 class Category(Base):
-    __tablename__ = 'categories'
+    __tablename__ = 'category'
 
-    category_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     title = Column(String(255), nullable=False)
-    expenses = relationship("Expense", back_populates="categories")
+    expenses = relationship('Expense', backref='category')
 
 
-def start_database(eng):
-    Base.metadata.create_all(eng)
+
+Base.metadata.create_all(engine)
