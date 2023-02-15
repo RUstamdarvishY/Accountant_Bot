483a6e7ced519b3071739782086ae89394d3f3b1
rustam <darvishi123@yandex.ru>
2023-02-15 18:37:00 +0300

    fixing fsm storage

--- create_bot.py
+++ create_bot.py
@@ -1,7 +1,10 @@
 from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from decouple import config
 
 
+storage = MemoryStorage()
+
 bot = Bot(token=config('BOT_TOKEN'))
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
--- states/expenses_states.py
+++ states/expenses_states.py
@@ -4,18 +4,16 @@ from aiogram import types
 from aiogram.dispatcher import FSMContext, Dispatcher
 from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
 
 from sqlalchemy.orm import sessionmaker
 
-from handlers.user_handlers import set_all_default_commands
 from utils.db_api.models import engine, Expense, Category
-from create_bot import dp
 from keyboards import currency_kb
 
 
-storage = MemoryStorage()
 Session = sessionmaker(bind=engine)
+
 
 class FSMExpense(StatesGroup):
     price = State()
@@ -23,14 +21,11 @@ class FSMExpense(StatesGroup):
     category = State()
 
 
-# @dp.message_handler(commands=['Добавить_расход'], state=None)
 async def fsm_start(message: types.Message):
     await FSMExpense.price.set()
     await message.reply('Введите цену')
-    await set_all_default_commands(message.bot, message.from_user.id)
 
 
-# @dp.message_handler(content_types=['text'], state=FSMExpense.price)
 async def get_price(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['price'] = message.text
@@ -38,7 +33,6 @@ async def get_price(message: types.Message, state: FSMContext):
     await message.reply('Введите валюту', reply_markup=currency_kb)
 
 
-# @dp.message_handler(content_types=['text'], state=FSMExpense.currency)
 async def get_currency(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['currency'] = message.text
@@ -46,36 +40,36 @@ async def get_currency(message: types.Message, state: FSMContext):
     await message.reply('Введите категорию расходов')
 
 
-# @dp.message_handler(content_types=['text'], state=FSMExpense.category)
 async def get_category(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['category'] = message.text
-        
+
     user_id = message.from_user.id
-    
+
     session = Session()
     price = data['price']
     currency = data['currency']
     category = data['category']
+
+    category_id = session.query(Category).filter(
+        Category.title == category).first()
     
-    category_id = session.query(Category).filter(Category.title == category).first()
-    
-    if not category_id.exists():
+
+    if category_id == None:
         new_category = Category(title=category)
         category_id = new_category.id
         session.add(new_category)
-            
-    expense = Expense(price, currency, category_id=category_id, user_id=user_id)
-        
+
+    expense = Expense(
+        price, currency, category_id=category_id, user_id=user_id)
+
     session.add(expense)
     session.commit()
-    
+
     await message.reply('Данные занесены в базу')
     await state.finish()
 
 
-# @dp.message_handler(state='*', commands=['Отмена'])
-# @dp.message_handler(Text(equals='Отмена', ignore_case=True), state='*')
 async def cancel(message: types.Message, state: FSMContext):
     current_state = await state.get_state()
     if current_state is None:
