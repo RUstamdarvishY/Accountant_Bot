4df810323696a5ca1e89f4c335d05afffa829e58
rustam <darvishi123@yandex.ru>
2023-02-20 16:09:58 +0300

    minor fixes

--- bot.py
+++ bot.py
@@ -16,8 +16,4 @@ expenses_states.register_expense_states_handlers(dp)
 email_states.register_email_states_handlers(dp)
 
 
-async def on_shutdown(_):
-    print('bot is offline')
-
-executor.start_polling(dp, skip_updates=True,
-                       on_startup=on_startup, on_shutdown=on_shutdown)
+executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
