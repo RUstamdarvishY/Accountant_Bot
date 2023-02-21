9c0959408d55a91ac9a5059c4135fbf9da1c41c0
rustam <darvishi123@yandex.ru>
2023-02-21 19:52:53 +0300

    rewrite bot from polling to webhook

--- bot.py
+++ bot.py
@@ -1,13 +1,16 @@
 from aiogram.utils import executor
 
-from create_bot import dp
+from create_bot import dp, bot
 from handlers import user_handlers
 from states import expenses_states, email_states
 
+from decouple import config
 
-async def on_startup(_):
+
+async def on_startup(dp):
     import middlewares
     middlewares.setup(dp)
+    await bot.set_webhook(config('APP_URL'))
     print('bot is online')
 
 
@@ -16,4 +19,14 @@ expenses_states.register_expense_states_handlers(dp)
 email_states.register_email_states_handlers(dp)
 
 
-executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+async def on_shutdown(dp):
+    await bot.delete_webhook(config('APP_URL'))
+
+executor.start_webhook(
+    dispatcher=dp,
+    webhook_path='',
+    on_startup=on_startup,
+    on_shutdown=on_shutdown,
+    skip_updates=True,
+    host="0.0.0.0",
+    port=config('PORT', default=5000))
