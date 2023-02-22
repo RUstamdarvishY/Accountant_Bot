e33cb6ee34ed16cb49eaab3ea5b737167d8e9774
rustam <darvishi123@yandex.ru>
2023-02-22 19:31:52 +0300

    fixing bugs with path to plots file

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -52,9 +52,9 @@ async def statistics_callback(callback: types.CallbackQuery):
     telegram_id = callback.from_user.id
 
     if callback.data == 'email':
-        plot = Path(Path.absolute('graphs.pdf'))
-        print(Path.absolute())
+        print(Path('graphs.pdf').absolute())
         print('!!!!!!!!!!!!!!!!!!')
+        plot = Path('graphs.pdf').absolute()
         if plot.is_file():
             plot.unlink()
         save_plots(f'graphs.pdf')
